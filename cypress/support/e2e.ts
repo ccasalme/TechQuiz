5b0042789ed13b3e27ae262303479cdb9fba7039
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-03-13 17:24:28 -0400

    e2e

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,7 @@
+// cypress/support/e2e.ts
+
+// Import Cypress commands (empty by default, but ready to extend!)
+import './commands';
+
+// You can put additional setup for e2e here clearly.
+// For example, global error handling or cookies management for e2e tests.
